01d8065f5c398320b3e182d9ac27c7ffeabd74f6
Anghy <anghyf27@hotmail.com>
2019-09-21 18:55:28 -0400

    fixing filters

--- src/pages/Landing/Pups/index.js
+++ src/pages/Landing/Pups/index.js
@@ -1,12 +1,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
-fetch('http://localhost:5000/allpets')
-  .then(data => data.json())
-  .then(pets => {
-    console.log(pets)
-  })
-
 const Container = styled.div`
   background-color: gray;
   display: flex;
@@ -63,7 +57,107 @@ const DropDown = styled.select`
   margin-right: 30px;
 `
 
+const PetContainer = styled.div`
+  border-right: 1px solid black;
+  border-bottom: 1px solid black;
+  border-left: 1px solid black;
+  padding: 10px;
+`
+
+const NoMatch = styled.div`
+  border: 1px solid blue;
+  background-color: lightblue;
+  color: blue;
+  text-align: center;
+  border-radius: 25px;
+  margin: 30px;
+`
+
 class AvailablePups extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      allPets: [],
+      type: 'All',
+      breed: 'All',
+      gender: 'All'
+    }
+
+    this.onChangeBreed = this.onChangeBreed.bind(this)
+    this.onChangeGender = this.onChangeGender.bind(this)
+    this.onChangeType = this.onChangeType.bind(this)
+  }
+
+  onChangeType(e) {
+    const valueType = e.target.value
+    this.setState({
+      type: valueType
+    })
+  }
+
+  onChangeBreed(e) {
+    const valueBreed = e.target.value
+    this.setState({
+      breed: valueBreed
+    })
+  }
+
+  onChangeGender(e) {
+    const valueGender = e.target.value
+    this.setState({
+      gender: valueGender
+    })
+  }
+
+  showPets() {
+    var newfilter = []
+    for (var i = 0; i < this.state.allPets.length; i++) {
+      if (this.state.allPets[i].gender === this.state.gender || this.state.gender === 'All') {
+        if (this.state.allPets[i].breed === this.state.breed || this.state.breed === 'All') {
+          if (this.state.allPets[i].type === this.state.type || this.state.type === 'All') {
+            newfilter.push(this.state.allPets[i])
+          } 
+        }
+      } else {
+        return (
+          <NoMatch>
+            <p>Sorry, there are no matches, try again!</p>
+          </NoMatch>
+        )
+      }
+    }
+
+    return (
+      <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap', marginTop: '20px',  backgroundColor: "beige" }}>
+        {newfilter.map(pet => {
+          return (
+            <div style={{margin: "10px 0"}}>
+              <img src={pet.image} alt={pet.name} height='250' width='250' />
+              <PetContainer>
+                <p style={{ margin: '0', paddingTop: '10px' }}>
+                  <strong>Name: </strong> {pet.name}
+                </p>
+                <p>
+                  <strong>Type: </strong>
+                  {pet.type}
+                </p>
+                <p>
+                  <strong>Breed: </strong>
+                  {pet.breed}
+                </p>
+                <p>
+                  <strong>Gender: </strong>
+                  {pet.gender}
+                </p>
+              </PetContainer>
+            </div>
+          )
+        })}
+      </div>
+    )
+  }
+
   render() {
     return (
       <div>
@@ -75,24 +169,45 @@ class AvailablePups extends Component {
           <FilterOptions>
             <Form>
               <Options>PET TYPE</Options>
-              <DropDown>
-                <option></option>
+
+              <DropDown onChange={this.onChangeType} value={this.state.type}>
+                <option value='All'>All</option>
+                {this.state.allPets.map(pet => {
+                  return <option key={pet._id}>{pet.type}</option>
+                })}
               </DropDown>
+
               <Options>BREED</Options>
-              <DropDown>
-                <option></option>
+              <DropDown onChange={this.onChangeBreed} value={this.state.breed}>
+                <option value='All'>All</option>
+                {this.state.allPets.map(pet => {
+                  return <option key={pet._id}>{pet.breed}</option>
+                })}
               </DropDown>
               <Options>GENDER</Options>
-              <DropDown>
-                <option>All Genders</option>
-                <option>Female</option>
-                <option>Male</option>
+              <DropDown onChange={this.onChangeGender} value={this.state.gender}>
+                <option value='All'>All</option>
+                {this.state.allPets.map(pet => {
+                  return <option key={pet._id}>{pet.gender}</option>
+                })}
               </DropDown>
             </Form>
           </FilterOptions>
         </Container>
+        {this.showPets()}
       </div>
     )
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:5000/allpets')
+      .then(data => data.json())
+      .then(pets => {
+        this.setState({
+          allPets: pets
+        })
+      })
+      .catch(err => console.log(err))
   }
 }
 
