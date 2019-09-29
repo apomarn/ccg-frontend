b340a08062a6cce3351aa2a9475876f08ca43157
Anghy <anghyf27@hotmail.com>
2019-09-29 18:40:05 -0400

    adding to the data.js

--- src/data.js
+++ src/data.js
@@ -51,5 +51,135 @@ export const allBreeds = [
     },
     trainability: '10%',
     barking: 'Barks If Necessary'
+  },
+  {
+    image:
+      'https://d2oe4uttdfvoiw.cloudfront.net/pictures/breeds/hEsFMZ3VfjmAI38fHeur/500x500-07_Beagle-1523993536226.png',
+    breed: 'beagle',
+    description:
+      'A sturdy hunting dog, the Beagle looks like a miniature foxhound. Its hunting ability, combined with a merry personality, has made the Beagle one of the most popular dogs in the United States, according to AKC Registration Statistics. The most famous Beagle of all is Snoopy from the comic strip "Peanuts." Today\'s Beagle comes in two height varieties (13 in. and 15 in.) and any true hound color, including tricolor, of red and white and lemon.',
+    history:
+      "The Beagle's history can be traced back to Ancient Greece. Beagles have had a variety of uses since the inception of the breed. Today, you can find Beagles as companions, therapy, hunting, search and rescue, and detection of various substances for law enforcement agencies.",
+    color: 'Any true hound color.',
+    temperament:
+      "Beagle puppies are gentle, sweet, lively and curious dogs that just loves everyone! Sociable, brave and intelligent, calm and loving. Excellent with children and generally good with other dogs. Beagles have minds of their own. They are determined and watchful and require patient, firm training. This breed doesn't like being left alone.",
+    adaptability: {
+      apartment: '80%',
+      owners: '60%',
+      alone: '20%',
+      weather: '50%',
+      friendly: '100%'
+    },
+    exercise: {
+      energy: '80%',
+      exercise: '80%',
+      playfulness: '100%'
+    },
+    trainability: '100%',
+    barking: 'Barks If Necessary'
+  },
+  {
+    image:
+      'https://d2oe4uttdfvoiw.cloudfront.net/pictures/breeds/EZcQX4302asGjO8xN9TC/500x500-10_Bichon-Frise-1542811699234.png',
+    breed: 'bichon frise',
+    description:
+      "A cheerful, happy dog, the Bichon Frise is small and sturdy with a dark-eyed inquisitive expression and a plumed tail it carries merrily over the back. The breed is often compared to a cotton ball due to its curled double coat, which consists of a textured outer coat and a silky undercoat. The Bichon Frise's coat must be white, but may have shadings of buff, cream or apricot around the ears or on the body.",
+    history:
+      'The Bichon Frise originated in the Mediterranean as far back as 300 B.C. This little dog once held the job of leader dog for the blind and circus performers prior to becoming the companion they are today.',
+    color: 'Their coat color is white, may have shadings of buff, cream or apricot around the ears or on the body.',
+    temperament:
+      'The Bichon Frise is gentle-mannered, sensitive, playful and affectionate. A cheerful attitude is the hallmark of the breed and one should settle for nothing less.',
+    adaptability: {
+      apartment: '100%',
+      owners: '100%',
+      alone: '20%',
+      weather: '60%',
+      friendly: '100%'
+    },
+    exercise: {
+      energy: '100%',
+      exercise: '40%',
+      playfulness: '100%'
+    },
+    trainability: '100%',
+    barking: 'Barks If Necessary'
+  },
+  {
+    image:
+      'https://d2oe4uttdfvoiw.cloudfront.net/pictures/breeds/5fbzjpE7mGcyAp3iyyDG/500x500-54_Chihuahua-1542819212932.png',
+    breed: 'chihuahua',
+    description:
+      'Graceful, alert and swift-moving with a saucy expression, the Chihuahua is highly intelligent and should not be underestimated despite its small size. The breed can be any color – solid, marked or splashed and the coat may be long or short. These sassy little dogs are well known as “purse dogs” like the famous Bruiser in the movie Legally Blonde starring Reese Witherspoon.',
+    history:
+      'The Chihuahua’s history is shrouded in mystery, but some believe it originated from the Fennec Fox, a very small animal with big eyes and ears, which could explain the tiny frame, luminous eyes and large ears on the breed today. Chihuahuas were used in religious ceremonies and were pets to the upper class. The breed derives its name from the Mexican State of Chihuahua, where the earliest specimens of the breed were found.',
+    color: 'All colors and markings.',
+    temperament:
+      'Chihuahua puppies are alert with terrier-like qualities. They are good with families if the children are gentle and patient. Because of their small size, they require little exercise and are good city dogs, but can be sensitive to cold temperatures. Smooth coated Chihuahuas need very little grooming due to their short hair. Long coats need occasional brushing but still require minimal grooming.',
+    adaptability: {
+      apartment: '100%',
+      owners: '80%',
+      alone: '20%',
+      weather: '20%',
+      friendly: '100%'
+    },
+    exercise: {
+      energy: '80%',
+      exercise: '20%',
+      playfulness: '80%'
+    },
+    trainability: '100%',
+    barking: 'Barks If Necessary'
+  },
+  {
+    image:
+      'https://d2oe4uttdfvoiw.cloudfront.net/pictures/breeds/jtDHajzbfM02sEJ4kVvP/500x500-55_Dalmatian-1542819280208.png',
+    breed: 'dalmatian',
+    description:
+      'The only spotted breed, the Dalmatian is alert and active, possessing great endurance, speed and intelligence. Their working and sporting heritage makes them suitable as both a family pet or performance animal, and they are often found in the show, obedience and agility rings, or galloping alongside a horse as a coach dog in “road trials.” Their short coat is white with black or liver (brown) spots.',
+    history:
+      'Spotted dogs have appeared throughout history in Europe, Asia, and Africa, credited with a dozen nationalities and as many native names. The dog we know today as the Dalmatian has been a dog of war, a draft dog, shepherd, ratter, fire-apparatus follower, firehouse mascot, bird dog, trail hound and retriever. Most importantly, it is the original and only coaching dog. Its affinity for horses remains a basic instinct to this day and the breed is a natural follower and guardian of the horse-drawn vehicle.',
+    color: 'The base color is white with black or dark brown round spots.',
+    temperament:
+      'The fun-loving, people-oriented Dalmatian thrives in a family environment. The breed’s short coat sheds almost year round, but regular brushing helps minimize the shedding. Dalmatian puppies are great with kids, but needs a little work with other dogs. It’s best to introduce the Dalmatian as a puppy to new environments.',
+    adaptability: {
+      apartment: '40%',
+      owners: '60%',
+      alone: '60%',
+      weather: '60%',
+      friendly: '100%'
+    },
+    exercise: {
+      energy: '100%',
+      exercise: '100%',
+      playfulness: '80%'
+    },
+    trainability: '50%',
+    barking: 'Barks If Necessary'
+  },
+  {
+    image:
+      'https://d2oe4uttdfvoiw.cloudfront.net/pictures/breeds/YsCd0GyUWysswKsEK9QM/500x500-65_Mini-Poodle-1542823704551.png',
+    breed: 'mini poodle',
+    description:
+      'The Mini Poodle, though often equated to the beauty with no brains, is exceptionally smart, active and excels in obedience training. He is also the only breed that comes in three size varieties. This size choice may be why the Poodle is one of the most popular breeds according to AKC Registration Statistics.',
+    history:
+      "Mini Poodles are believed to be of German descent, originally called Pudelhund, but their true history still remains a mystery. The German name roughly translates to splash dogs, named because of their swimming and water retrieval capabilities. Since the breed's creation, France has adopted it as its national dog.",
+    color: 'Coat colors come in black, gray, white, and brown.',
+    temperament:
+      'The Mini Poodle is considered one of the most intelligent breeds. They learn quickly and are easy to train. Mini Poodle puppies are also quite sensitive and have long memories. They are wonderfully well-mannered house puppies and typically friendly with other pets.',
+    adaptability: {
+      apartment: '100%',
+      owners: '100%',
+      alone: '20%',
+      weather: '60%',
+      friendly: '100%'
+    },
+    exercise: {
+      energy: '80%',
+      exercise: '80%',
+      playfulness: '100%'
+    },
+    trainability: '100%',
+    barking: 'Barks If Necessary'
   }
 ]
